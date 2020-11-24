5084387a49adb5d68930ab38ec935b7dac8ccc3f
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-11-24 17:39:02 +0700

    Test client

--- Tests/Clients/ConsoleApp/Program.cs
+++ Tests/Clients/ConsoleApp/Program.cs
@@ -10,12 +10,12 @@ namespace ConsoleApp
         {
             StatusAsync().Wait();
             ServerTimeAsync().Wait();
+            MarketSymbolsAsync().Wait();
         }
 
         static async Task StatusAsync()
         {
             BitkubOpenAPI.API api = new BitkubOpenAPI.API();
-
             var result = await api.GetStatusAsync();
             Console.WriteLine(result);
         }
@@ -23,8 +23,14 @@ namespace ConsoleApp
         static async Task ServerTimeAsync()
         {
             BitkubOpenAPI.API api = new BitkubOpenAPI.API();
-
             var result = await api.GetServerTimeAsync();
+            Console.WriteLine(result);
+        }
+
+        static async Task MarketSymbolsAsync()
+        {
+            BitkubOpenAPI.Market market = new BitkubOpenAPI.Market();
+            var result = await market.GetSymbolsAsync();
             Console.WriteLine(result);
         }
     }
